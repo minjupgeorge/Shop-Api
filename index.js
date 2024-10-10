e7c0ab5de1df043a6b11856e8e883bd71d8559e3
minjupgeorge <minjupgeorge>
2024-10-10 12:48:48 +0200

    products updated

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ import cookieParser from 'cookie-parser';
 import logger from './middleware/logger.js';
 
 // import routes
+import userRoutes from './routes/user.js';
+import shopRoutes from './routes/shop.js';
 
 // load environment variables
 dotenv.config();
@@ -37,6 +39,8 @@ app.set('views', path.join(PATH, 'views'));
 app.use(logger);
 
 // use routes
+app.use('/api',shopRoutes);
+app.use('/api',userRoutes);
 
 // handle 404
 app.use('*', (req, res) => {
